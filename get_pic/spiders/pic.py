a3714974aeb6c9ee1a6152ae472d35bc8cd7410b
zyy54895 <549334141@qq.com>
2018-05-14 23:04:30 +0800

    增加翻页功能

--- get_pic/spiders/pic.py
+++ get_pic/spiders/pic.py
@@ -8,15 +8,24 @@ class PicSpider(scrapy.Spider):
     allowed_domains = ['www.mmjpg.com']
     start_urls = ['http://www.mmjpg.com']
 
+    def pic_parse(self, response):
+        pass
+
     def parse(self, response):
         soup = BeautifulSoup(response.text, 'lxml')
         # print(soup.find_all('ul'))
         for ul in soup.find_all('ul'):
             # print(ul.find_all('li'))
             contents = ul.find_all('li')
+            contents_dict = {}
         for i in range(len(contents)):
-            content_url = re.findall('<a.href="(http.*?/\d+)".*?</a>', str(contents[i]))
-            print(content_url)
+            content = re.findall('<a.href="(http.*?/\d+)".*?<img\salt="(.*?)".*?</a>', str(contents[i]))[0]
+            #print(content)
+            contents_dict[content[1]] = content[0]
+        print(contents_dict)
+        for key in contents_dict.keys():
+            yield scrapy.Request(url=contents_dict[key], callback=self.pic_parse)
+
 
 
 
